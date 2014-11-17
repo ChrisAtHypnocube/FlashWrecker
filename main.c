5da651941767e4987267074796d83c5a7c1584d3
chris <chris@lomont.org>
2014-11-17 18:20:19 -0500

    finished code

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 
 #include <xc.h>   // standard part port definitions
 #include <plib.h> // peripherial definitions
+#include <stdbool.h>
+#include <stdint.h>  // basic types like uint16_t, etc.
 
 
 #define SYS_CLOCK 48000000L   // system clock speed
@@ -16,6 +18,8 @@
 
 #define TEXT_SIZE 100 // size of input/output buffer
 
+// place to format messages
+char text[TEXT_SIZE];
 
 
 /**********************
@@ -56,7 +60,7 @@
 #endif
 
 //The desired startup baudRate
-#define DESIRED_BAUDRATE   (9600)
+#define DESIRED_BAUDRATE   (1000000)
 UINT16 clockDivider = SYS_CLOCK/(4*DESIRED_BAUDRATE) - 1;
 
 // if there is any UART error, return 1, else return 0
@@ -74,6 +78,23 @@ int UARTError()
 
         return err!=0?1:0;
 }
+
+
+// read byte if one is ready.
+// if exists, return true and byte
+// if return false, byte = 0 is none avail, else
+// byte != 0 means error
+bool UARTReadByte(uint8_t * byte)
+    {
+    if (UARTReceivedDataIsAvailable(UART1))
+    {
+        *byte = UARTGetDataByte(UART1);
+        return true;
+    }
+    *byte = 0;
+    return false;
+    } // UARTReadByte
+
 
 // blocking call to write byte to UART
 void UARTWriteByte(char byte)
@@ -165,6 +186,34 @@ void InitializeUART(int clock)
 
 /*********************** FLASH CODE *******************************************/
 
+// value of an erased flash word.
+// Bits can be flipped away from this state without requiring a page reset
+// used to tell if a word has not been written
+#define FLASH_ERASED_WORD_VALUE 0xFFFFFFFF
+
+
+#if defined(__32MX150F128C__) || defined(__32MX150F128B__)
+#define FLASH_MODEL_ROW_SIZE_WORDS    32   // # of 32-bit Instructions per Row
+#define FLASH_MODEL_PAGE_SIZE_WORDS  256   // # of 32-bit Instructions per Page
+#endif
+
+#ifdef BYTE_PAGE_SIZE
+#undef BYTE_PAGE_SIZE
+#endif
+#define BYTE_PAGE_SIZE ((FLASH_MODEL_PAGE_SIZE_WORDS)*4)
+
+// size of FLASH to test in bytes
+#define FLASH_BLOCK_SIZE_BYTES (BYTE_PAGE_SIZE*16) // # of pages in flash
+
+// a frame is how much we test at once
+#define PAGES_PER_FRAME 2
+// number of frames
+#define FRAMES (FLASH_BLOCK_SIZE_BYTES/BYTE_PAGE_SIZE)/(PAGES_PER_FRAME)
+
+// 32 bit words per frame
+#define WORDS_PER_FRAME ((PAGES_PER_FRAME)*((BYTE_PAGE_SIZE)/4))
+
+
 // Note that:
  //    "bytes" needs to be a multiple of BYTE_PAGE_SIZE (and aligned that way) if you intend to erase
  //    "bytes" needs to be a multiple of BYTE_ROW_SIZE (and aligned that way) if you intend to write rows
@@ -173,17 +222,7 @@ void InitializeUART(int clock)
      __attribute__ ((aligned(align),section(".text,\"ax\",@progbits #"))) = \
      { [0 ...(bytes)-1] = 0xFF }
 
-NVM_ALLOCATE(RomData,BYTE_PAGE_SIZE,STATIC_BLOCK_SIZE);  // allocates "RomData" of 8k bytes, aligned on erasable page boundary
-
-// all system settings - keep a copy here for eash reference
-// int FlashPageErasureCount, todo - how does this counter work?
-// long Blits
-// long mainLoopPasses;
-// unsigned int profileInterruptTime;
-// long mainLoopPasses;
-// long completePowerUps;
-// long completePowerDowns;
-// int interruptTime;
+NVM_ALLOCATE(FlashData,BYTE_PAGE_SIZE,FLASH_BLOCK_SIZE_BYTES);  // allocates "RomData" of 8k bytes, aligned on erasable page boundary
 
 UINT32 flashErasedCount = 0;
 UINT32 flashBadChecksums = 0;
@@ -191,125 +230,95 @@ UINT32 flashNoBlocksFound = 0;
 UINT32 flashCannotGetBlockErrors = 0;
 UINT32 flashWriteFailedCount = 0;
 
-// call this to erase all settings
-void FlashErase()
+// map a virtual address to a physical address.
+// needed for FLASH writing and erasing routines
+// needed for any hardware peripherals
+// todo - move to utils.h ultimately
+void * VirtualToPhysicalAddress(const void * virtualAddress)
 {
-    flashErasedCount++;
+    uint32_t addr = (uint32_t)(virtualAddress);
+    return (void*)(addr & 0x1FFFFFFF);
 
-    // erase pages
-    int index = 0;
-    while (index < STATIC_BLOCK_SIZE)
-    {
-        NVMErasePage((void*)(RomData + index)); // ignore return codes here
-        index += BYTE_PAGE_SIZE;
-    }
+    /* from http://ww1.microchip.com/downloads/en/DeviceDoc/61115F.pdf
+    3.4.1 Virtual to Physical Address Calculation (and Vice-Versa)
+
+    1. virtual kernel KSEG0 or KSEG1 to physical:
+    ? Physical Address = Virtual Address and 0x1FFFFFFF
+    2. physical to KSEG0 virtual
+    ? KSEG0 Virtual Address = Physical Address | 0x80000000
+    3. physical to KSEG1 virtual
+    ? KSEG1 Virtual Address = Physical Address | 0xA0000000
+
+    4. To translate from KSEG0 virtual to KSEG1 virtual address
+    ? KSEG1 Virtual Address = KSEG0 Virtual Address | 0x20000000
+
+    */
 }
 
 
-// read flash storage block. Return 1 on success, else 0
-// size is at most 126 bytes
-// on failure buffer is 0 filled
-int FlashReadBlock(UINT8 * buffer, int size)
+
+// call this to erase pages
+// return true on success, else false
+bool FlashErase(uint32_t startOffset, uint32_t length)
 {
-    int index;
-
-    if ((size < 0) || (MAX_BUFFER_SIZE < size))
-        return 0; // out of bounds failure
-
-    // zero buffer
-    for (index = 0; index < size; ++index)
-        buffer[index] = 0;
-    // find last free good one
-    index = STATIC_BLOCK_SIZE-128;
-    while (index >= 0)
+    uint32_t offset = startOffset;
+    uint32_t endOffset = startOffset + length;
+    int errors = 0;
+    while (offset < endOffset)
     {
-        if (RomData[index] < 128)
-        { // block here - if checksum is good, we're done
-            int index2, checksum=0;
-            int goodChecksum = RomData[index]*256+RomData[index+1];
-            for (index2 = 0; index2 < size; index2++)
-                checksum += RomData[index+index2+2];
-            checksum &= (1<<CHECKSUM_BITS)-1; // mask off unused bits
-            if (checksum == goodChecksum)
-            { // good entry. Return it
-                for (index2 = 0; index2 < size; ++index2)
-                    buffer[index2] = RomData[index+index2+2];
-                return 1;
-            }
-            else
+        uint32_t * ptr = (void*)FlashData;
+        ptr   += offset;
+        void * physicalAddress = VirtualToPhysicalAddress(ptr);
+
+        // Claim: Microchip has serious bug in their PIC32 peripheral lib - erase page depends on series
+        // from http://www.microchip.com/forums/m772388.aspx
+        // next solution: In "PIC32 Flash Programming Specification" DS61145L, Table 5.1: I finally found the details.
+        // Flash page size 256 Word for PIC32MX1 and MX2. Flash page size 1024 Words for MX3 and higher, and 4096 Words for MZ devices.
+        // shizzle fuck :)
+
+        // needs PHYSICAL address, not virtual address
+        if (NVMErasePage(physicalAddress) != 0)
+        {   // double try
+            if (NVMErasePage(physicalAddress) != 0)
             {
-                flashBadChecksums++;
+                errors++;
             }
         }
-        index -= 128; // try previous one
+        offset += BYTE_PAGE_SIZE/4; // note this size changes by PIC model
+
     }
-    flashNoBlocksFound++;
-    return 0; // none found
+    if (errors == 0)
+        return true;
+    return false;
 }
 
-// write flash storage block. Return 1 on success, else 0
-// size is at most 126 bytes
-int FlashWriteBlock(UINT8*buffer, int size)
+
+uint32_t FlashRead(uint32_t offset)
 {
-    UINT8 buf2[128];
-    int index;
-    int index2, checksum=0;
-    unsigned int word;
-
-    if ((size < 0) || (MAX_BUFFER_SIZE < size))
-        return 0; // out of bounds failure
-
-    // try and find free row index
-    index = 0;
-    while ((index < STATIC_BLOCK_SIZE) && (RomData[index] < 128))
-        index += 128; // try next one
-
-    if (index >= STATIC_BLOCK_SIZE)
-    { // went over the top, time to clear the flash block, and start over
-        FlashErase();
-
-        // try and find free row index, this time failure is failure :)
-        index = 0;
-        while ((index < STATIC_BLOCK_SIZE) && (RomData[index] < 128))
-            index += 128; // try next one
-
-        if (index >= STATIC_BLOCK_SIZE)
-        {
-            flashCannotGetBlockErrors++;
-            return 0; // off the top. Must be an error
-        }
-    }
-
-    // zero temp buffer
-    memset(buf2,0,128);
-
-    // create checksum and prepare temp buffer
-    for (index2 = 0; index2 < size; ++index2)
-    {
-        checksum += buffer[index2];
-        buf2[index2+2] = buffer[index2];
-    }
-    checksum &= (1<<CHECKSUM_BITS)-1;
-    buf2[0] = (checksum>>8);
-    buf2[1] = (checksum&255);
-
-    // write the data 4 bytes at a time, checksum part last
-    for (index2 = 128-4; index2 >= 0; index2 -= 4)
-    {
-        word  = buf2[index2+3] << 24;
-        word += buf2[index2+2] << 16;
-        word += buf2[index2+1] <<  8;
-        word += buf2[index2  ]      ;
-        if (NVMWriteWord((void*)(RomData+index+index2),word) != 0)
-        {
-            flashWriteFailedCount++;
-            return 0; // failed
-        }
-    }
-
-    return 1; // success
+    uint32_t * ptr = (void*)FlashData;
+    return *(ptr+offset);
 }
 
+// return true on success, else false
+bool FlashWrite(uint32_t wordOffset, uint32_t value)
+{
+    if (FLASH_BLOCK_SIZE_BYTES / 4 <= wordOffset)
+        return false; // out of bounds
+
+    uint32_t * ptr = (void*)FlashData;
+    ptr += wordOffset;
+    void * physicalAddress = VirtualToPhysicalAddress(ptr);
+
+    // try writing twice, and check the value each try
+    if (NVMWriteWord(physicalAddress, value) != 0 || *ptr != value)
+    {
+        if (NVMWriteWord(physicalAddress, value) != 0 || *ptr != value)
+        {
+            return false;
+        }
+    }
+    return true;
+}
 
 /*********************** main code ********************************************/
 
@@ -367,7 +376,29 @@ void Initialize()
         //PrintSerial(text);
 }
 
-char text[TEXT_SIZE];
+// RAM buffer to track recent reads
+uint32_t ramBufferErased[WORDS_PER_FRAME];
+uint32_t ramBufferWritten[WORDS_PER_FRAME];
+
+void InitRamFrame()
+{
+    PrintSerial("Resetting RAM copy.\r\n");
+    int i;
+    for (i = 0; i < WORDS_PER_FRAME; ++i)
+    {
+        ramBufferErased[i]  = FLASH_ERASED_WORD_VALUE;
+        ramBufferWritten[i] = ~FLASH_ERASED_WORD_VALUE;
+    }
+}
+
+void OutputError(uint8_t charType, uint32_t offset, uint32_t readWord, uint32_t desiredWord)
+{
+    snprintf(text,TEXT_SIZE,"ERROR: (%c) offset %08X read %08X desired %08X.\r\n",charType,offset,readWord,desiredWord);
+    PrintSerial(text);
+}
+
+uint32_t passes[FRAMES];
+
 
 int main(void)
 {
@@ -376,9 +407,93 @@ int main(void)
     sprintf(text,"\r\n\r\nHypnocube FLASH destroyer %d.%d\r\n",VERSION>>4, VERSION&15);    
     PrintSerial(text);
 
-    
+    sprintf(text,"Testing %d frames of %d bytes each.\r\n",FRAMES,WORDS_PER_FRAME*4);
+    PrintSerial(text);
 
+    sprintf(text,"Flash starts at 0x%08x.\r\n",FlashData);
+    PrintSerial(text);
 
+    int frame = 0;
+    int ramFrame = -1;
+    int i;
+    uint32_t errors = 0;
+
+    WriteCoreTimer(0);
+    while (1)
+    {
+        uint32_t startOffset = WORDS_PER_FRAME*frame;
+
+        if (ramFrame != frame)
+        {
+            InitRamFrame();
+            ramFrame = frame;
+        }
+
+        ++passes[frame];
+
+        // message for start
+        uint32_t time = ReadCoreTimer();
+        sprintf(text,"Pass %d, frame %d, offset %08x, time %08x, errors %d\r\n",
+                passes[frame],frame,startOffset,time,errors
+                );
+        PrintSerial(text);
+
+        // erase frame
+        if (!FlashErase(startOffset,WORDS_PER_FRAME))
+            PrintSerial("ERROR: flash erase failed.\r\n");
+
+        // check all entries are FLASH_ERASED_WORD_VALUE
+        for (i = 0; i < WORDS_PER_FRAME; ++i)
+        {
+            uint32_t word = FlashRead(startOffset+i);
+            if (ramBufferErased[i] != word)
+            { // an error, output, and set buffer
+                OutputError('E',startOffset+i,word,ramBufferErased[i]);
+                ramBufferErased[i] = word;
+                ++errors;
+            }
+        }
+
+        // write all ~FLASH_ERASED_WORD_VALUE
+        for (i = 0; i < WORDS_PER_FRAME; ++i)
+        {
+            if (!FlashWrite(startOffset+i,~(FLASH_ERASED_WORD_VALUE)))
+            {
+                snprintf(text,TEXT_SIZE,"ERROR: flash write failed ar offset %08X.\r\n",startOffset+i);
+                PrintSerial(text);
+            }
+        }
+
+        // check all entries are FLASH_ERASED_WORD_VALUE
+        for (i = 0; i < WORDS_PER_FRAME; ++i)
+        {
+            uint32_t word = FlashRead(startOffset+i);
+            if (ramBufferWritten[i] != word)
+            { // an error, output, and set buffer
+                OutputError('W',startOffset+i,word,ramBufferWritten[i]);
+                ramBufferWritten[i] = word;
+                ++errors;
+            }
+        }
+
+        // check if frame changed
+        uint8_t byte;
+        if (UARTReadByte(&byte))
+        {
+            snprintf(text,TEXT_SIZE,"Command received %c [%02x].\r\n",byte,byte);
+            PrintSerial(text);
+            if ('1' <= byte && byte <= FRAMES+'1'-1)
+            { // change frame
+                frame = byte-'1';
+                snprintf(text,TEXT_SIZE,"Switching to frame %d.\r\n",frame);
+                PrintSerial(text);
+            }
+            if (byte == 'q')
+                break;
+        }
+    }
+
+    PrintSerial("Quitting...\r\n");
     return 0;
 }
 
